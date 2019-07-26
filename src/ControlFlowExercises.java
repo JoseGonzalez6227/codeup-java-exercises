b23cabc23a739165702e627c80598f12a6ad4fc1
Your Name <you@example.com>
2019-07-26 15:20:44 -0500

    Flow exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //        TODO:Loop Basics
@@ -35,9 +37,54 @@ public class ControlFlowExercises {
         for(long y = 2; y < 1000000; y*=y){
             System.out.println(y);
         }
+
+//        TODO:Fizzbuzz
+        for(int a = 1; a <= 100; a++){
+            if(a % 15 == 0){
+                System.out.println("FizzBuzz");
+            }else if(a % 5 == 0){
+                System.out.println("Buzz");
+            }else if(a % 3 == 0){
+                System.out.println("Fizz");
+            }else {
+                System.out.println(a);
+            }
+        }
+//        TODO:Display a table of powers.
+        System.out.println("Please enter an integer");
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");
+        int userInput = sc.nextInt();
+        System.out.println("Do you want to continue?");
+        String answer = sc.next();
+        System.out.println("Here is your table!");
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+        if(answer.equalsIgnoreCase("yes")){
+            for(int b = 1; b <= userInput; b++){
+                int squared = b*b;
+                int cubed = b*b*b;
+                System.out.printf("%-7d|%-9d|%-6d%n",b,squared,cubed);
+            }
+        }
+//        TODO:Convert given number grades into letter grades.
+        System.out.println("Please enter the student's grade:");
+        int grade = sc.nextInt();
+        System.out.println("Do you want to continue?");
+        String userAnswer = sc.next();
+        if(userAnswer.equalsIgnoreCase("yes")){
+            if(grade >= 88){
+                System.out.println("You got an A! Excellent job.");
+            }else if(grade >= 80 && grade <= 87){
+                System.out.println("You got a B! Great job");
+            }else if(grade >= 67 && grade <= 79){
+                System.out.println("You got a C. You passed");
+            }else if(grade >= 60 && grade <= 66){
+                System.out.println("You got a D. Please review.");
+            }else {
+                System.out.println("You got an F. See your instructor.");
+            }
+        }
     }
 }
 
-//        TODO:Fizzbuzz
-//        TODO:Display a table of powers.
-//        TODO:Convert given number grades into letter grades.
+
